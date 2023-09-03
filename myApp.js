be544483d3344bff8055b39ff7c6847e0d6077db
tim_hokty <timhokty.planb@gmail.com>
2023-09-03 18:22:32 +0700

    Add public path

--- myApp.js
+++ myApp.js
@@ -3,6 +3,7 @@ let app = express();
 
 console.log("Hello World")
 
+app.use("/public", express.static(__dirname + "/public"));
 app.get('/', function(req, res) {
     let absolutePath = __dirname + '/views/index.html'
     res.sendFile(absolutePath);
