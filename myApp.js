7ca6733ca740f3491815a9f401ea9c6869c232fd
tim_hokty <timhokty.planb@gmail.com>
2023-09-03 18:51:52 +0700

    Chain Middleware to Create a Time Server

--- myApp.js
+++ myApp.js
@@ -24,7 +24,7 @@ app.get("/now", function(req, res, next) {
     req.time = new Date().toString();
     next();
 }, function(req, res) {
-    res.send(req.time);
+    res.json({"time": req.time});
 })
 
 
