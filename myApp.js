baec14e1d259149768b5a39afca270b5091708f0
tim_hokty <timhokty.planb@gmail.com>
2023-09-03 18:50:57 +0700

    Chain Middleware to Create a Time Server

--- myApp.js
+++ myApp.js
@@ -20,6 +20,12 @@ app.get('/json', function(req, res) {
     res.json({"message": process.env.MESSAGE_STYLE == 'uppercase' ? string.toUpperCase() : string})
 });
 
+app.get("/now", function(req, res, next) {
+    req.time = new Date().toString();
+    next();
+}, function(req, res) {
+    res.send(req.time);
+})
 
 
 
