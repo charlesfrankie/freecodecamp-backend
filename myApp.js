ff565dec2b6ef4d7b9cef009cf751079dc3285f3
tim_hokty <timhokty.planb@gmail.com>
2023-09-05 11:17:03 +0700

    Create a Model CRUD Part I - CREATE

--- myApp.js
+++ myApp.js
@@ -6,6 +6,13 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 .catch((err) => console.log(err));
 let Person;
 
+const personSchema  = new mongoose.Schema({
+  name: {type: String, required: true},
+  age:  {type: Number, required: false},
+  favoriteFoods: {type: String, required: false}
+});
+Person = personSchema;
+
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
 };
